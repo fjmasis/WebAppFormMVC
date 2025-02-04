463705ef870199d6c862d3ae48ddc29d34393bac
fjmasis <102482854+fjmasis@users.noreply.github.com>
2025-02-03 19:30:10 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppFormMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
